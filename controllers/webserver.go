04d6fb4b6e3bd246c54a668fab3b8efd2d58742f
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-02-19 01:04:56 +0900

    Add:設定の追加

--- controllers/webserver.go
+++ controllers/webserver.go
@@ -6,27 +6,15 @@ import (
     "github.com/gorilla/mux"
     "io/ioutil"
     "log"
+    "techtrain_go_development/config"
+    "techtrain_go_development/models"
     "net/http"
-    "time"
+    "strconv"
 )
 
-type ItemParams struct {
-    Id           string    `json:"id"`
-    JanCode      string    `json:"jan_code,omitempty"`
-    ItemName     string    `json:"item_name,omitempty"`
-    Price        int       `json:"price,omitempty"`
-    CategoryId   int       `json:"category_id,omitempty"`
-    SeriesId     int       `json:"series_id,omitempty"`
-    Stock        int       `json:"stock,omitempty"`
-    Discontinued bool      `json:"discontinued"`
-    ReleaseDate  time.Time `json:"release_date,omitempty"`
-    CreatedAt    time.Time `json:"created_at"`
-    UpdatedAt    time.Time `json:"updated_at"`
-    DeletedAt    time.Time `json:"deleted_at"`
+type DeleteResponse struct {
+    Id string `json:"id"`
 }
-
-// ポインタ型でitemsを定義します。今回はこのグローバル変数【配列】がデータベースの役割をします
-var items []*ItemParams
 
 func rootPage(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Welcome to the Go Api Server")
@@ -34,80 +22,93 @@ func rootPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func fetchAllItems(w http.ResponseWriter, r *http.Request) {
+    var items []models.Item
+    // modelの呼び出し
+    models.GetAllItems(&items)
+    responseBody, err := json.Marshal(items)
+    if err != nil {
+        log.Fatal(err)
+    }
+
     w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(items)
+    w.Write(responseBody)
 }
 
 func fetchSingleItem(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
     vars := mux.Vars(r)
-    key := vars["id"]
+    id := vars["id"]
 
-    for _, item := range items {
-        if item.Id == key {
-            json.NewEncoder(w).Encode(item)
-        }
+    var item models.Item
+    // modelの呼び出し
+    models.GetSingleItem(&item, id)
+    responseBody, err := json.Marshal(item)
+    if err != nil {
+        log.Fatal(err)
     }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(responseBody)
 }
 
 func createItem(w http.ResponseWriter, r *http.Request) {
     reqBody, _ := ioutil.ReadAll(r.Body)
-    var item ItemParams
+
+    var item models.Item
     if err := json.Unmarshal(reqBody, &item); err != nil {
         log.Fatal(err)
     }
+    // modelの呼び出し
+    models.InsertItem(&item)
+    responseBody, err := json.Marshal(item)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-    items = append(items, &item)
-    json.NewEncoder(w).Encode(item)
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(responseBody)
 }
 
 func deleteItem(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
     id := vars["id"]
 
-    for index, item := range items {
-        if item.Id == id {
-            items = append(items[:index], items[index+1:]...)
-        }
+    // modelの呼び出し
+    models.DeleteItem(id)
+    responseBody, err := json.Marshal(DeleteResponse{Id: id})
+    if err != nil {
+        log.Fatal(err)
     }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(responseBody)
 }
 
 func updateItem(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
     id := vars["id"]
-
     reqBody, _ := ioutil.ReadAll(r.Body)
-    var updateItem ItemParams
+
+    var updateItem models.Item
     if err := json.Unmarshal(reqBody, &updateItem); err != nil {
         log.Fatal(err)
     }
-
-    for index, item := range items {
-        if item.Id == id {
-            items[index] = &ItemParams{
-                Id:           item.Id,
-                JanCode:      updateItem.JanCode,
-                ItemName:     updateItem.ItemName,
-                Price:        updateItem.Price,
-                CategoryId:   updateItem.CategoryId,
-                SeriesId:     updateItem.SeriesId,
-                Stock:        updateItem.Stock,
-                Discontinued: updateItem.Discontinued,
-                ReleaseDate:  updateItem.ReleaseDate,
-                CreatedAt:    item.CreatedAt,
-                UpdatedAt:    updateItem.UpdatedAt,
-                DeletedAt:    item.DeletedAt,
-            }
-        }
+    // modelの呼び出し
+    models.UpdateItem(&updateItem, id)
+    convertUintId, _ := strconv.ParseUint(id, 10, 64)
+    updateItem.Model.ID = uint(convertUintId)
+    responseBody, err := json.Marshal(updateItem)
+    if err != nil {
+        log.Fatal(err)
     }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(responseBody)
 }
 
-// 先頭を「大文字」にすると外部ファイルから読み込めるようになります。（export）
 func StartWebServer() error {
     fmt.Println("Rest API with Mux Routers")
     router := mux.NewRouter().StrictSlash(true)
 
-    // router.HandleFunc({ エンドポイント }, { レスポンス関数 }).Methods({ リクエストメソッド（複数可能） })
     router.HandleFunc("/", rootPage)
     router.HandleFunc("/items", fetchAllItems).Methods("GET")
     router.HandleFunc("/item/{id}", fetchSingleItem).Methods("GET")
@@ -116,39 +117,5 @@ func StartWebServer() error {
     router.HandleFunc("/item/{id}", deleteItem).Methods("DELETE")
     router.HandleFunc("/item/{id}", updateItem).Methods("PUT")
 
-    return http.ListenAndServe(fmt.Sprintf(":%d", 8080), router)
-}
-
-// モックデータを初期値として読み込みます
-func init() {
-    items = []*ItemParams{
-        &ItemParams{
-            Id:           "1",
-            JanCode:      "327390283080",
-            ItemName:     "item_1",
-            Price:        2500,
-            CategoryId:   1,
-            SeriesId:     1,
-            Stock:        100,
-            Discontinued: false,
-            ReleaseDate:  time.Now(),
-            CreatedAt:    time.Now(),
-            UpdatedAt:    time.Now(),
-            DeletedAt:    time.Now(),
-        },
-        &ItemParams{
-            Id:           "2",
-            JanCode:      "3273902878656",
-            ItemName:     "item_2",
-            Price:        1200,
-            CategoryId:   2,
-            SeriesId:     2,
-            Stock:        200,
-            Discontinued: false,
-            ReleaseDate:  time.Now(),
-            CreatedAt:    time.Now(),
-            UpdatedAt:    time.Now(),
-            DeletedAt:    time.Now(),
-        },
-    }
+    return http.ListenAndServe(fmt.Sprintf(":%d", config.Config.ServerPort), router)
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.15
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16 // indirect
-	gopkg.in/ini.v1 v1.62.0 // indirect
+	gopkg.in/ini.v1 v1.62.0
 )
